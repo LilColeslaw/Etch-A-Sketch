df718125af7940a92ce2fe7e69d5dc631ee165cc
Winslow Solomon <winsolo11@gmail.com>
2022-07-02 21:02:53 -0400

    Add the shading mode and the necessary functions to make it work

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ const container = document.querySelector(".container");
 let amDrawing = false;
 const body = document.querySelector("body");
 const changeDimensionsButton = document.querySelector(".dimension-change");
+let usingRandomColors = true;
 
 body.addEventListener("keypress", (event) => {if (event.key === " ") toggleDrawing();});
 
-function rgbRandom() {
+function setCanvasColor() { //needed to set the canvas color when it's resized or first created
+    let boxes = document.querySelectorAll(".box");
+    boxes.forEach((box) => box.style.cssText = "background-color: black; opacity: 0");
+}
+
+function useShading() {//blurs the button and changes the mode of coloring
+    usingRandomColors = false;
+    this.blur();
+}
+function useRandomColors() {//blurs the button and changes the mode of coloring
+    usingRandomColors = true;
+    this.blur();
+}
+
+function changeToOnEnter(event) {//used when mouseEnter -> sees what mode has been chosen and calls the appropriate function
+    let targetElement = event.target;
+    usingRandomColors ? rgbRandom(targetElement) : shade(targetElement);
+}
+
+function shade(element) {
+    element.style.opacity = parseFloat(element.style.opacity) + 0.1;
+}
+
+function rgbRandom(element) {//gets random color and sets the passed element's background color as it
     let r = Math.floor(Math.random() * 256);
     let g = Math.floor(Math.random() * 256);
     let b = Math.floor(Math.random() * 256);
     let rgb = `rgb(${r}, ${g}, ${b})`;
-    return rgb;
+    element.style.backgroundColor = rgb;
+    element.style.opacity = 100;//needed because if the shade mode was being used these colors wouldn't show up well
 }
+
 function clear() {
     container.textContent = "";
 }
 
-function createGrid(num) {
+function createGrid(num) {//creates the grid
     for (let i = 0; i < num; i++) {
         let rowDiv = document.createElement("div");
         container.appendChild(rowDiv);
@@ -32,7 +58,7 @@ function createGrid(num) {
     }
 }
 
-function changeDimensions() {
+function changeDimensions() {//collects new side length and sets it
     let sideLength = prompt("Enter the side length: ");
     if (sideLength % 1 !== 0) {
         alert("You have not entered a valid number.");
@@ -43,31 +69,30 @@ function changeDimensions() {
         changeDimensionsButton.blur();
         return;
     }
-    clear();
-    createGrid(sideLength);
-    addDrawing();
-    changeDimensionsButton.blur();
+    clear();//clears the current grid so things don't get weird
+    createGrid(sideLength);//creates the grid
+    setCanvasColor();//sets the color to default
+    addDrawing();//initiates drawing
+    changeDimensionsButton.blur();//blurs the button
 }
 
-function addDrawing() {
+function addDrawing() {//initiates drawing for every box
     const box = document.querySelectorAll(".box");
-    box.forEach((box) => box.addEventListener("mouseenter", bgColorRandom));
+    box.forEach((box) => box.addEventListener("mouseenter", changeToOnEnter));
     amDrawing = true;
 }
 
-function bgColorRandom(event) {
-    event.target.style.backgroundColor = rgbRandom();
-}
 
-function removeDrawing() {
+function removeDrawing() {//turns of drawing for every box
     const box = document.querySelectorAll(".box");
-    box.forEach((box) => box.removeEventListener("mouseenter", bgColorRandom));
+    box.forEach((box) => box.removeEventListener("mouseenter", changeToOnEnter));
     amDrawing = false;
 }
 
-function toggleDrawing() {
+function toggleDrawing() {//toggles the drawing
     amDrawing ? removeDrawing() : addDrawing();
 }
 
 createGrid(16);
+setCanvasColor();
 addDrawing();
